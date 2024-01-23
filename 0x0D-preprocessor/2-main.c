c42f3c36d42bd3c0c5ab51ebd3577ca2975f9c83
mariamihabmo <mariam.ihab.mo@gmail.com>
2024-01-23 22:30:04 +0200

    jgj

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main point
+ * main - Entry point
+ *
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
  *
  * Return: Always 0 (Success)
 */
